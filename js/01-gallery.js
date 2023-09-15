45783591c8b224734d43eaabe8eb9909b50e77dc
Tap0kk <129516260+Tap0kk@users.noreply.github.com>
2023-09-15 17:54:08 +0300

    end second task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+list.addEventListener("click", handleClick);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  const imgUrl = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <img src="${imgUrl}" width="1280" height="auto"/>
+    `,
+    {
+      onShow: (instance) => {
+        window.addEventListener(`keydown`, onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener(`keydown`, onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = "Escape";
+    const isEscKey = event.code === ESC_KEY_CODE;
+    if (!isEscKey) return;
+    instance.close();
+  }
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    </li>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  overlayOpacity: 0.7,
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
