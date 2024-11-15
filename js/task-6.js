5518131a80f0bd20e0471de95d15fc310e873ea6
Deman <deman2805@gmail.com>
2024-11-15 20:54:10 +0200

    change task 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,16 +1,36 @@
-"use strict"
+'use strict';
 
-const input = document.querySelector("input");
-const create = document.querySelector("button[data-create]");
-const destroy = document.querySelector("button[data-destroy]")
-const box = document.querySelector("#boxes")
+const input = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
 
-create.addEventListener("click", createMarkup);
+createBtn.addEventListener('click', markup);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-function createMarkup() {
+function markup() {
   if (input.value < 1 || input.value > 100) {
-    return
+    return alert('Too many boxes');
   }
+  destroyBoxes();
+  const boxesArray = [];
+  for (let i = 0; i < input.value; i += 1) {
+    boxesArray.push(createBoxes(i));
+  }
+  input.value = '';
+  boxes.append(...boxesArray);
+}
+
+function createBoxes(amount) {
+  const box = document.createElement('div');
+  box.style.width = `${30 + amount * 10}px`;
+  box.style.height = `${30 + amount * 10}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  return box;
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
 }
 
 function getRandomHexColor() {
